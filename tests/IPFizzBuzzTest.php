e76fffe0b0e5936b2d509f66dd1d508698102eb7
Luan P. Coelho <luan@byteslivres.com.br>
2020-04-19 18:58:07 -0300

    First Test

--- tests/IPFizzBuzzTest.php
+++ tests/IPFizzBuzzTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Exception\InvalidIP4Format;
 use PHPUnit\Framework\TestCase;
 
 class IPFizzBuzzTest extends TestCase
@@ -19,5 +20,30 @@ class IPFizzBuzzTest extends TestCase
     public function shouldReturnFizz()
     {
         $this->assertEquals("Fizz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.3"));
+    }
+
+    /**
+     * @test
+     */
+     public function shouldReturnBuzz()
+     {
+        $this->assertEquals("Buzz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.5"));
+     }
+
+    /**
+     * @test
+     */
+    public function shouldReturnFizzBuzz()
+    {
+        $this->assertEquals("FizzBuzz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.15"));
+    }
+
+    /**
+     * @test
+     */
+    public function notAllPartsTest()
+    {
+        $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0");
+        //$this->expectException(InvalidIP4Format::class);
     }
 }
