8795d9c5cee55b29f055a6bf791917ca625d2369
Luan P. Coelho <luan@byteslivres.com.br>
2020-04-20 23:33:40 -0300

    Test IPFinder

--- tests/IPFinderTest.php
+++ tests/IPFinderTest.php
@@ -4,11 +4,27 @@ namespace App\Service;
 
 use PHPUnit\Framework\TestCase;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+use App\Service\IPFinder;
+
 class IPFinderTest extends TestCase
 {
-
-    private function getUp(): void
+    /**
+     * @test
+     */
+    public function checkFindIp()
     {
+
+       $mock = new MockHandler([new Response(200, [], '127.0.0.1')]);
+       $handler = HandlerStack::create($mock);
+       $guzzleClient = new Client(['handler' => $handler]);
+
+        $ipFinder = new IPFinder($guzzleClient);
+        $this->assertEquals("127.0.0.1", $ipFinder->findIp());
 
     }
 }
